0d1313f6f85cd1e7b538e03bfeaf43b856679bbe
andreaarro <andreaarroyoduran@hotmail.com>
2025-08-10 00:38:56 -0600

    imagenes

--- festispot/lib/paginas/carousel_pages.dart
+++ festispot/lib/paginas/carousel_pages.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Inicio extends StatelessWidget {
+class Inicio extends StatefulWidget {
   const Inicio({super.key});
 
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
